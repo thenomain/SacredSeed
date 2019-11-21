1f452dd3d2487ebb7daf83c22d1b5b94eeb5ad8b
Thenomain <thenomain@gmail.com>
2019-11-21 17:37:14 -0500

    cleanup + "chance die" success counter

--- commands/thenojunk.py
+++ commands/thenojunk.py
@@ -28,14 +28,17 @@ class CmdWoDRoll(default_cmds.MuxCommand):
     def func(self):
         "roll things"
 
+        # NO ARGS
         if not self.args:
             self.caller.msg("Nothing passed")
             return
 
-        pool = 0
+        # BUILD POOL (# of dice to roll)
         value = self.lhs
+        pool = 0
+        min_success = 8
 
-        # is `value` a number or trait?
+        # For some reason the following ValueErrors on `int(1.6)`, though `@py int(1.6)` returns 2. Huh.
         try:
             value = int(value)  # valid for things like "+1" and "-100"
             pool += self.calculate_number(value)
@@ -43,17 +46,19 @@ class CmdWoDRoll(default_cmds.MuxCommand):
             pool += self.calculate_trait(value)
 
         if pool > CONST_MAX_POOL:
-            self.caller.msg("Too many dice.")
-            return 0
+            self.caller.msg("Too many dice")
+            return
 
         # ROLL
-        self.caller.msg("> # of dice ---> %i" % pool)
         result = self.roll_pool(pool)
-        self.caller.msg("> result --> %r" % result)
+        # self.caller.msg("> # of dice ---> %i" % pool)
+        # self.caller.msg("> result --> %r" % result)
 
         # SUCCESS COUNTER
         # this thing is apparently called "list comprehension" and it's cool
-        success_values = [x for x in result if x >= 8]
+        if pool <= 0:  # chance die
+            min_success = 10
+        success_values = [x for x in result if x >= min_success]
         successes = len(success_values)
         self.caller.msg("> successes --> %r (%r)" % (success_values, successes))
 
@@ -62,7 +67,7 @@ class CmdWoDRoll(default_cmds.MuxCommand):
         # message = "Rolling " + str(value)
         result_pretty = []
         for x in result:
-            if x >= 8:
+            if x >= min_success:
                 result_pretty.append(f"|h{x}|n")
             else:
                 result_pretty.append(f"|x{x}|n")
@@ -79,13 +84,13 @@ class CmdWoDRoll(default_cmds.MuxCommand):
         self.caller.msg(message)
 
     def calculate_number(self, value):
-        "validate a single integer value"
-        self.caller.msg("> number: %s" % value)
+        "validate a single integer value; probably needs to do nothing"
+        # self.caller.msg("> number: %s" % value)
         return value
 
     def calculate_trait(self, trait):
         "validate a single string; assume trait and take apart"
-        self.caller.msg("> trait: %s" % trait)
+        # self.caller.msg("> trait: %s" % trait)
         return 0
 
     def roll_pool(self, pool):
@@ -116,9 +121,3 @@ class CmdWoDRoll(default_cmds.MuxCommand):
             return [die] + self.roll_die(again)
         else:
             return [die]
-
-
-# if 10 in dice_roll_list:
-#     do_dice_stuff()
-
-# list1 = list.append(*list2) <-- * unpacks the list :: [a,b,c] -> a,b,c
