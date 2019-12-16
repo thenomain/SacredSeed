5d47be7e824c2f25702ea5c967a809cad43779b7
Thenomain <thenomain@gmail.com>
2019-12-16 17:16:16 -0500

    easier to read, allow to exclude disconnected peeps

--- thenocode/finders.py
+++ thenocode/finders.py
@@ -1,4 +1,4 @@
-def build_targets_list(self, targets_string):
+def build_targets_list(self, targets_string, include_disconnected=True):
     """
     Function to build a target list on the following criteria:
         "here" returns the location object of the enactor
@@ -7,11 +7,12 @@ def build_targets_list(self, targets_string):
     Args:
         self: class object of Command or MuxCommand type
         targets_string: space- or comma-delimited list of names
+        include_disconnected: should disconnected characters on list be kept? (default: True)
 
     Returns:
         list[] of objects found, 'here' returning caller's containing room
-
     """
+
     caller = self.caller
 
     if "," in targets_string:
@@ -19,9 +20,16 @@ def build_targets_list(self, targets_string):
     else:
         delimiter = " "
 
-    targets = [target.strip() for target in targets_string.split(delimiter)]  # remove spaces around each element
-    targets = [caller.search(target) for target in set(targets)]  # search each element, report misfires, remove repeats
-    targets = [target for target in targets if target]  # remove empty elements
+    targets = [t.strip() for t in targets_string.split(delimiter)]  # remove spaces around each element
+    targets = [caller.search(t) for t in set(targets)]  # search each element, report misfires, remove repeats
+    targets = [t for t in targets if t]  # remove empty elements
+
+    if not include_disconnected:
+        for i, t in enumerate(targets):
+            if (not t.is_connected) or (not t == caller.location):
+                caller.msg(f"{t} is not connected")
+                targets.remove(i)
+        # targets = [t for t in targets if (not t.is_connected) or (not t == caller.location)]
 
     if (caller.location not in targets) and (caller not in targets):
         targets += [caller]
